79045a111e43c8aeeaf9606a1d7894e3aace7437
Pooja Raval <poojaraval055@gmail.com>
2021-01-07 16:34:22 +0530

    The final tasks of week 1

---
+++ abc.py
@@ -0,0 +1,42 @@
+"""
+def pooja():
+    print("hello")
+
+
+pooja()
+
+def p1(a):
+    b = a+10
+    return b
+
+print(p1(10))
+
+def sqr(a):
+    b = a * a
+    return b
+
+print(sqr(5))
+
+def sqrt(i):
+     j = i ** 2
+     return j
+
+print(sqrt(5))
+"""
+
+"""
+def change_char(s1):
+    ch = s1[0]
+    s1 =  s1.replace (ch , '$')
+    s1 = ch + s1[1:]
+    return s1
+print (change_char('restart'))
+
+
+def person(p1,p2,p3):
+    print("the youngest person is:" + p3)
+
+person(p1 = "pooja",p2 = "payal",p3 = "diya")
+"""
+def prime(i):
+    
